60ed17b2fa112f3dec998241592dc630c9402621
Robert Bendun <robert@bendun.cc>
2021-12-02 23:24:45 +0100

    Day 2 part 1

---
+++ 2-natalie/2a.rb
@@ -0,0 +1,15 @@
+x, y = 0, 0
+
+File.new('test-data').readlines().each do |line|
+  dir, val = line.split()
+  xmul, ymul = 0, 0
+
+  if dir == "forward" then xmul = 1 end
+  if dir == "up" then ymul = -1 end
+  if dir == "down" then ymul = 1 end
+
+  x += val.to_i * xmul
+  y += val.to_i * ymul
+end
+
+puts "X: #{x}, Y: #{y}, Mul: #{x*y}"
---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2021
+
+Each day in different programming language:
+
+- Day 1: [Bash](https://www.gnu.org/software/bash/)
+- Day 2: [Natalie](https://natalie-lang.org/), Ruby implementation, compiled to C++, by Tim Morgan.
